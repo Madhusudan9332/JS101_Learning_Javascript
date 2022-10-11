341bc56feb866885e4e63bdafda31d866be38b81
unknown <madhusudangautam003@gmail.com>
2022-10-11 20:10:30 +0530

    Web23 Variables

---
+++ problam2.js
@@ -0,0 +1,6 @@
+let name = "Madhusudan Gautam";
+console.log(name);
+name = "Devendra Kumar Gautam";
+console.log(name);
+name = "KhemLata";
+console.log(name);
---
+++ problam6.js
@@ -0,0 +1,4 @@
+let x = 16 ;
+let y = 1/2;
+
+console.log(x**y);
